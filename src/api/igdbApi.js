6ec17969179bd66d059b87a59b901215c1d82842
Dariusz Gawron <screbsy+g1@gmail.com>
2022-08-24 22:33:24 +0200

    Prepare configuration for igdb api

--- src/api/igdbApi.js
+++ src/api/igdbApi.js
@@ -2,5 +2,59 @@ import axiosConfig from "./axiosConfig";
 import igdbConfig from "./igdbconfig";
 
 const igdbApi = {
-    
-}
+    getGames: (queryParams) => {
+        const url = 'games';
+        return axiosConfig.post(url, queryParams);
+    },
+    getGenres: (queryParams) => {
+        const url = 'genres';
+        return axiosConfig.post(url, queryParams);
+    },
+    getVideos: (queryParams) => {
+        const url = 'game_videos';
+        return axiosConfig.post(url, queryParams);
+    },
+    getScreenshots: (queryParams) => {
+        const url = 'screenshots';
+        return axiosConfig.post(url, queryParams);
+    },
+    getCovers: (queryParams) => {
+        const url = 'covers';
+        return axiosConfig.post(url, queryParams);
+    },
+    getPlatforms: (queryParams) => {
+        const url = 'platforms';
+        return axiosConfig.post(url, queryParams);
+    },
+    getPlatformLogos: (queryParams) => {
+        const url = 'platform_logos';
+        const data = queryParams ? queryParams : 'fields *;' 
+        return axiosConfig.post(url, data);
+    },
+    getCharacters: (queryParams) => {
+        const url = 'characters';
+        return axiosConfig.post(url, queryParams);
+    },
+    getAgeRatings: (queryParams) => {
+        const url = 'age_ratings';
+        return axiosConfig.post(url, queryParams);
+    },
+    getWebsites: (queryParams) => {
+        const url = 'websites';
+        return axiosConfig.post(url, queryParams);
+    },
+    getReleaseDates: (queryParams) => {
+        const url = 'release_dates';
+        return axiosConfig.post(url, queryParams);
+    },
+    getThemes: (queryParams) => {
+        const url = 'themes';
+        return axiosConfig.post(url, queryParams);
+    },
+    searchGames: (queryParams) => {
+        const url = 'search';
+        return axiosConfig.post(url, queryParams);
+    }
+};
+
+export default igdbApi;
--- src/api/igdbConfig.js
+++ src/api/igdbConfig.js
@@ -1,7 +1,7 @@
 const igdbConfig = {
     baseApiUrl: 'https://api.igdb.com/v4',
     clientId: process.env.REACT_APP_IGDB_CLIENT_ID,
-    clientSecret: process.env.REACT_APP_IGDB_CLIENT_SECRET
+    clientSecret: process.env.REACT_APP_IGDB_ACCESS_TOKEN
 };
 
 export default igdbConfig;
