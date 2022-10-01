7655e207074101c5e11c48f7c2300e237d9be343
Dariusz Gawron <screbsy+g1@gmail.com>
2022-10-01 22:20:13 +0200

    Corrections + preparation of the display of grades

--- src/components/GameGrid/GameGrid.js
+++ src/components/GameGrid/GameGrid.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import igdbApi from "../../api/igdbApi";
 import igdbConfig from "../../api/igdbConfig";
 
+import Loader from "../Loader/Loader";
 import GameCard from "../GameCard/GameCard";
 import Button from "../Button/Button";
 
@@ -10,6 +11,8 @@ import "./GameGrid.scss";
 
 const GameGrid = props => { 
     const [games, setGames] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
 
@@ -18,51 +21,67 @@ const GameGrid = props => {
             const query = (props.keyword)
                 ? `search "${props.keyword}"; fields *, cover.*, platforms.*, platforms.platform_logo.*; limit ${igdbConfig.gridItems};`
                 : `fields *, cover.*, platforms.*, platforms.platform_logo.*; sort rating desc; limit ${igdbConfig.gridItems};`;
-            const gamesResponse = await igdbApi.getGames(query);
-            const pagesResponse = await igdbApi.getGames(query,'T');
-            setGames(gamesResponse);
-            setTotalPages(Math.ceil(pagesResponse.count/igdbConfig.gridItems));
+            try {
+                const gamesResponse = await igdbApi.getGames(query);
+                const pagesResponse = await igdbApi.getGames(query,'T');
+                setGames(gamesResponse);
+                setTotalPages(Math.ceil(pagesResponse.count/igdbConfig.gridItems));
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+            
         };
         getGames();
     }, [props.keyword]);
 
     const loadMore = async () => {
-        const page = currentPage+1;
-        setCurrentPage(page);
-        const offset = ((page-1) * igdbConfig.gridItems) + 1;
+        // setLoading(true);
+        const page = currentPage + 1;
+        const offset = ((page - 1) * igdbConfig.gridItems) + 1;
         const query = (props.keyword)
             ? `search "${props.keyword}"; fields *, cover.*, platforms.*, platforms.platform_logo.*; offset ${offset}; limit ${igdbConfig.gridItems};`
             : `fields *, cover.*, platforms.*, platforms.platform_logo.*; sort rating desc; offset ${offset}; limit ${igdbConfig.gridItems};`;
-        const response = await igdbApi.getGames(query);
-        setGames([...games,...response]);
+        try {
+            const response = await igdbApi.getGames(query);
+            setGames([...games,...response]);
+            setCurrentPage(page);
+        } catch (error) {
+            setError(error);
+        } finally {
+            // setLoading(false);
+        }
     }
 
     return (
         <div className="game-grid section">
         {
-            games && games.length>0
-            ?   <>
-                    <div className="game-grid__container">
-                    {        
-                        games.map((game, index) => {
-                            return <GameCard game={game} key={index}/>
-                        })
-                    }
+            loading 
+            ?   <Loader />
+            :   games && games.length>0
+                ?   <>
+                        <div className="game-grid__container">
+                        {        
+                            games.map((game, index) => {
+                                return <GameCard game={game} key={index}/>
+                            })
+                        }
+                        </div>
+                        {
+                            totalPages!==0 && currentPage<totalPages && (
+                                <Button onClick={loadMore} class='button--primary button--center'>
+                                    Load more
+                                </Button>
+                            )
+                        }
+                    </>
+                :   <div className="game-grid__empty">
+                        <i className="game-grid__empty-icon"></i>
+                        <p className="game-grid__empty-text">
+                            No data
+                        </p>
                     </div>
-                    {
-                        totalPages!==0 && currentPage<totalPages && (
-                            <Button onClick={loadMore} class='button--primary button--center'>
-                                Load more
-                            </Button>
-                        )
-                    }
-                </>
-            :   <div className="game-grid__empty">
-                    <i className="game-grid__empty-icon"></i>
-                    <p className="game-grid__empty-text">
-                        No data
-                    </p>
-                </div>
         }
         </div>
     )
